91567b3c9e8d3c10970a2de0f2880e405d2ee122
tiki3SI3 <116684041+tiki3SI3@users.noreply.github.com>
2023-03-08 14:45:23 +0100

    Update asm02.s

--- asm02.s
+++ asm02.s
@@ -3,45 +3,45 @@
 rubrique .bss
     entrée RESB 3 ; réserve 3 octets
     len EQU $-entrée ; longueur d'entrée
-section .données
+section .data
     base de messages '1337', 10 ; initialisation
     chiffre db '42', 10
 
 section .texte
     global_start
-    _commencer:
+    _start:
 
         mov eax, 3 ; lecture de l'appel système
         mov ebx, 0 ; descripteur de fichier 0 = stdin
         mov ecx, entrée ; destination
         mov edx, len ; longueur
-        entier 0x80
+        int 0x80
 
 
         mov esi, chiffre
         mov edi, entrée
         xor edx, edx
-        BOUCLE:
-            mobile, [esi + edx]
+        LOOP:
+            mov al, [esi + edx]
             mov bl, [edi + edx]
             inc edx
             cmp al, bl ; comparaison de char
             jne not_equal
-            cmpbl, 10; Fin de la chaine ?
-            je suis égal42
-            BOUCLE jmp
+            cmp bl, 10; Fin de la chaine ?
+            je equal42
+            jmp LOOP
 
         égal42 :
             mov ebx, 1 ; 1 = sortie standard (stdout)
             mov ecx, message ; message à afficher
-            déplacer eax, 4 ; 4 = fonction écrire
+            mov eax, 4 ; 4 = fonction write
             mouv edx, 4 + 1 ; longueur du message 4 caractères de '1337'
-            entier 0x80
+            int 0x80
             déplacer eax, 1
             mov ebx, 0
-            entier 0x80
+            int 0x80
 
         inégal:
-            déplacer eax, 1
+            mov eax, 1
             mov ebx, 1
-            entier 0x80
+            int 0x80
