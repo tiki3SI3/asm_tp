f5ef663cb5a8481113114e8b8514a41f55998b2f
tiki3SI3 <116684041+tiki3SI3@users.noreply.github.com>
2023-03-08 14:41:40 +0100

    Update asm01.s

--- asm01.s
+++ asm01.s
@@ -8,10 +8,10 @@ _start:
     déplacer eax, 4 ; code pour l'appel système écrire
     mov ebx, 1 ; descripteur de fichier stdout
     mov ecx, message ; pointeur vers le message
-    mouv edx, 4+1 ; longueur du message
-    entier 0x80 ; appel système écrire
+    mov edx, 4+1 ; longueur du message
+    int 0x80 ; appel système écrire
 
 
-    déplacer eax, 1 ; rencontré eax à 1
+    mov eax, 1 ; rencontré eax à 1
     mov ebx, 0
-    entier 0x80   
+    int 0x80   
