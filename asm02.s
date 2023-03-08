8a92a6684bb77b0eebf5e15bd01161476ad9b7b7
tiki3SI3 <116684041+tiki3SI3@users.noreply.github.com>
2023-03-08 14:51:39 +0100

    Update asm02.s

--- asm02.s
+++ asm02.s
@@ -1,25 +1,25 @@
 ; Affiche 1337 et retourne 0 si il reçoit 42 en entrée sinon 1
 
-rubrique .bss
-    entrée RESB 3 ; réserve 3 octets
-    len EQU $-entrée ; longueur d'entrée
+section .bss
+    input RESB 3 ; reserver 3 Bytes
+    len EQU $-input ; longueur de input
 section .data
-    base de messages '1337', 10 ; initialisation
+    message db '1337', 10 ; initialisation 
     chiffre db '42', 10
 
-section .texte
-    global_start
+section .text
+    global _start
     _start:
 
-        mov eax, 3 ; lecture de l'appel système
-        mov ebx, 0 ; descripteur de fichier 0 = stdin
-        mov ecx, entrée ; destination
+        mov eax, 3 ;syscall read
+        mov ebx, 0 ; file descriptor 0 = stdin
+        mov ecx, input ; destination
         mov edx, len ; longueur
         int 0x80
 
 
         mov esi, chiffre
-        mov edi, entrée
+        mov edi, input
         xor edx, edx
         LOOP:
             mov al, [esi + edx]
@@ -27,21 +27,21 @@ section .texte
             inc edx
             cmp al, bl ; comparaison de char
             jne not_equal
-            cmp bl, 10; Fin de la chaine ?
+            cmp bl, 10 ; fin de la chaine ?
             je equal42
             jmp LOOP
 
-        égal42 :
+        equal42:
             mov ebx, 1 ; 1 = sortie standard (stdout)
             mov ecx, message ; message à afficher
-            mov eax, 4 ; 4 = fonction write
-            mouv edx, 4 + 1 ; longueur du message 4 caractères de '1337'
+            mov eax, 4 ;  4 = fonction write
+            mov edx, 4 + 1 ; longueur du message 4 charctere de '1337' 
             int 0x80
-            déplacer eax, 1
+            mov eax, 1
             mov ebx, 0
             int 0x80
 
-        inégal:
+        not_equal:
             mov eax, 1
             mov ebx, 1
             int 0x80
