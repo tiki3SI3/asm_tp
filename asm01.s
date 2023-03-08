8b21d68ac7d28a6836652241d7b7467fa8153999
tiki3SI3 <116684041+tiki3SI3@users.noreply.github.com>
2023-03-08 14:52:33 +0100

    Update asm01.s

--- asm01.s
+++ asm01.s
@@ -1,17 +1,30 @@
 section .data
-    base de données de messages '1337', 10
+    message db '1337', 10section .data
+    message db '1337', 0
 
-global_start
+global _start
  
-section .texte
+section .text
 _start:
-    déplacer eax, 4 ; code pour l'appel système écrire
-    mov ebx, 1 ; descripteur de fichier stdout
+    mov eax, 4 ; code pour l'appel système write
+    mov ebx, 1 ; file descriptor stdout
     mov ecx, message ; pointeur vers le message
-    mov edx, 4+1 ; longueur du message
-    int 0x80 ; appel système écrire
+    mov edx, 4 ; longueur du message
+    int 0x80 ; appel système write
+
+    xor eax, 0 ; met eax à 0
+    ret ; retourne le contenu de eax, qui est 0
 
 
-    mov eax, 1 ; rencontré eax à 1
-    mov ebx, 0
-    int 0x80   
+global _start
+ 
+section .text
+_start:
+    mov eax, 4 ; code pour l'appel système write
+    mov ebx, 1 ; file descriptor stdout
+    mov ecx, message ; pointeur vers le message
+    mov edx, 4 ; longueur du message
+    int 0x80 ; appel système write
+
+    xor eax, eax ; met eax à 0
+    ret ; retourne le contenu de eax, qui est 0
