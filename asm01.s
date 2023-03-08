97f0f9eb8d84b29af1578fa6a86494365a66b1a0
tiki3SI3 <116684041+tiki3SI3@users.noreply.github.com>
2023-03-08 17:04:36 +0100

    Update asm01.s

--- asm01.s
+++ asm01.s
@@ -10,6 +10,7 @@ _start:
     mov ebx, 1 ; file descriptor stdout
     mov ecx, message ; pointeur vers le message
     mov edx, 4 ; longueur du message
+
     int 0x80 ; appel système write
     xor eax, 0 ; met eax à 0
     ret ; retourne le contenu de eax, qui est 0
@@ -17,13 +18,4 @@ _start:
 
 global _start
  
-section .text
-_start:
-    mov eax, 4 ; code pour l'appel système write
-    mov ebx, 1 ; file descriptor stdout
-    mov ecx, message ; pointeur vers le message
-    mov edx, 4 ; longueur du message
-    int 0x80 ; appel système write
 
-    xor eax, eax ; met eax à 0
-    ret ; retourne le contenu de eax, qui est 0
